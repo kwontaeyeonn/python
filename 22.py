b0cd737833bbca6c1fb5626892ea517ee117f5da
kwontaeyeon0310 <kwontaeyeon2009@gmail.com>
2025-05-22 20:26:45 +0900

    python

--- 22.py
+++ 22.py
@@ -1,7 +1,33 @@
-str = "Hello, my friends!"
+arr = []
 
-if 'a' in str:
-    print(str)
+for i in range(4):
+	row = list(map(int, input().split()))
+	arr.append(row)
 
-else:
-    print("없습니다.")
+for i in range(4):
+	hap = 0
+	for j in range(2):
+		hap = arr[i][j]
+	print(hap//2, end=" ")
+print()
+
+for i in range(2):
+	hap = 0
+	for j in range(4):
+		hap += arr[j][i]
+	print(hap//2, end=" ")
+print()
+
+hap = 0
+for i in range(4):
+	for j in range(4):
+		hap += arr[i][j]
+print(hap//8)
+
+n,m = map(int,input().split())
+arr = []
+for i in range(n):
+	row = []
+	for j in range(m):
+		# txt = f'({i}, {j})'
+		row.append((i, j))
--- test/test1.py
+++ test/test1.py
@@ -1,23 +1,86 @@
-num1 = 3
-num2 = 10
+# class Car:
+#     def __init__(self,displ,drv):
+#         self.displ = displ
+#         self.drv = drv
 
-num1 = int(num1)
-num = int(num2)
-result = num1+num2
+#         def info(self):
+#             print(f'자동차정보:{self.displ}, {self.drv} 구동')
 
-print(num1, '+', num2, '=', result)
 
-print("저는 대구소프트웨어마이스터고등학교 학생 입니다.")
-a=1
-print("저는", a, "학년 입니다.")
+# car_ obj = car(3000, '4륜')
+# car_obj.info()
 
-p, score = input().split()
-p = float(p)
-score = int(score)
+# class TV:
+#     def __init__(self,channel,volume,on):
+#         self.channel = channel
+#         self.volum = volume
+#         self.on = o
 
-print(p*score)
+#     def show(self):
+#         print(self.channel, self.volum, self.on)
+#     def setChannel(self,channel):
+#         self.channel = channel
+    
+#     def getChannel(self, channel):
+#         return self.channel 
 
-K, M, E, P = map(int, input().split())
-avg = (K+M+E+P)/4
-avg = int(avg)
-print(avg)
+#67
+# class Counter:
+#     def __init__(self):
+#         self.cnt = 0
+
+#     def reset(self,initValue):
+#         self.cnt = initValue
+
+#     def increment(self):
+#         self.cnt += 1
+
+#     def get(self):
+#         return self.cnt
+    
+# a = Counter()
+# b = Counter()
+
+# a.rest(0)
+# a.increment()
+# print(a.get())
+
+# b.rest(100)
+# b.increment()
+# print(b.get())
+class BankAccount:
+    def __init__(self,acnt):
+        self.acnt = acnt
+        self.balance = 0
+
+    def deposit(self,amt):
+        print(f'통장에 {amt}원이 입금됨')
+        self.balance += amt
+        print(f'현재 잔액은 {self.balance}원\n')
+        return self.balance
+    
+    def withdraw(self,amt):
+        print(f'통장에 {amt}원이 출금됨')
+        self.balance -= amt
+        print(f'현재 잔액은 {self.balance}원\n')
+        return self.balance
+    
+    def transfer(self,target,amt):
+        if amt>self.balance:
+            print("잔액이 부족합니다")
+        else:
+            target.balance += amt
+            self.balance -= amt
+            print(f'{self.acnt}님이 {target.acnt}님과 {amt}원 이체했습니다.')
+            print(f'{target.acnt}의 잔액 {target.balance}')
+    
+a = BankAccount('123-456')
+a.deposit(10000)
+a.withdraw(5000)
+
+b = BankAccount('456-789')
+b.deposit(2000)
+
+a.check()
+a.transfer(b,100)
+a.check()
