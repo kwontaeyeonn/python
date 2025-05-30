3ea71bd2c645d75279147f07db3963fb38b220a7
kwontaeyeon0310 <kwontaeyeon2009@gmail.com>
2025-05-30 21:33:29 +0900

    python

--- main/74.파일쓰기.py
+++ main/74.파일쓰기.py
@@ -1,11 +1,11 @@
 #74
-f = open('data_1.txt', 'w')
+# f = open('data_1.txt', 'w')
 
-for i in range(1,11):
-    line = f'{i}번째 줄\n'
-    f.write(line)
+# for i in range(1,11):
+#     line = f'{i}번째 줄\n'
+#     f.write(line)
 
-f.close()
+# f.close()
 
 #75
 f = open('data_1.txt', 'a')
